f1aaa48c83cf115795f9c3b6102730305fa91f40
codewithprashant <guptaps@mail.uc.edu>
2024-12-28 17:30:55 -0500

    updated projects

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,13 +1,21 @@
 export const projects = [
  
-  {
+    {
         id: 1,
+        name: 'Python BDD UI Automation',
+        date: '2024',
+        tech: ['Python', 'Gherkin','Behave', 'Selenium'],
+        tagline:
+            'This project provides a generic framework for implementing UI test automation using Python, Selenium, and BDD (Behavior-Driven Development)',
+        source: 'https://github.com/codewithprashantgupta/python-bdd-ui-automation'
+    },
+    {
+        id: 2,
         name: 'Coming Soon',
         date: '',
         tech: [],
         tagline:
-            'Projects getting updated soon...',
-
-  }
+            'More projects will be added soon—stay tuned for updates!',
+    } 
   ];
   
